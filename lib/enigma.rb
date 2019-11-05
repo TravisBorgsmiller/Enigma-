c1f476bb41132bd6f915aaf06381034ee864de3c
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-11-04 21:23:21 -0700

    Add initial version of encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,10 @@
 class Enigma
 
+  def encrypt(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
 
-end 
+
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,14 +2,24 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 require './lib/enigma'
+require 'date'
 
 class EnigmaTest < MiniTest::Test
 
   def test_does_enigma_exist
 
-    new_enigma = Enigma.new 
+    enigma = Enigma.new
     assert_instance_of Enigma, new_enigma
   end
 
+  def test_does_message_encrypt
+
+    enigma = Enigma.new
+    require "pry"; binding.pry
+    enigma.encrypt("hello world", "02715", "040895")
+    assert_equal "hello world", enigma.message
+    assert_equal "02715", enigma.key
+    assert_equal "040895", enigma.date
+  end
 
 end
